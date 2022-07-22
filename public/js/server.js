e482821d91377c08d4dd71611ffc2e16767ff756
Vincent Huynh <vlh@uoregon.edu>
2022-07-21 18:08:28 -0700

    rearranging directory structure

---
+++ public/js/server.js
@@ -0,0 +1,37 @@
+const express = require('express'); 
+const path = require('path');
+const fileupload = require('express-fileupload');
+
+let initial_path = path.join(__dirname, "public");
+
+const app = express(); 
+
+app.use(express.static(initial_path)); 
+app.use(fileupload());
+
+app.get('/', (req, res) => { 
+  res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () => {
+  console.log('listening.....');
+})
+
+app.post('/upload', (req, res) => {
+  let file = req.files.image; 
+  let date = new Date(); 
+  //image name 
+  let imagename = date.getDate() + date.getTime() + file.name;
+  //image upload path 
+  let path = 'public/uploads' + imagename; 
+
+  //create upload 
+  file.mv(path, (err, result) => {
+    if(err){
+      throw errl
+    } else {
+       res.json(`uploads/${imagename}`)
+    }
+  })
+})
+
