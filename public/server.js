3f4f0e85ae4213dae8be57a003bc2417b875a046
Vincent Huynh <vlh@uoregon.edu>
2022-07-22 14:13:04 -0700

    server.js updates

--- public/server.js
+++ public/server.js
@@ -3,8 +3,8 @@
 
 //create express app
 const express = require('express'); 
-const sqlite3 = require('sqlite3')
 
+var db = require("./js/database.js");
 var app = express();
 
 //server port
@@ -12,12 +12,28 @@ var HTTP_PORT = 8000
 
 //start server 
 app.listen(HTTP_PORT, () => {
-  console.log("server running on port %PORT%".replace(%PORT%, HTTP_PORT))
+  console.log("server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
 //Root endpoint 
 app.get("/", (req, res, next) => {
   res.json({"message":"OK"})
+});
+
+
+//get all blogposts from the DB
+app.get("/api/blogpost", (req, res, next) => {
+  var sql = "select * from blogpost";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if(err) {
+      res.status(400).json({"error":err.message});
+      return;
+    } else{
+      res.status(200).json(rows);
+    }
+  });
+
 });
 
 
